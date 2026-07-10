4c3a6de167e944f6f28e3b7d07c4944b999966c2
Joshua Lucas <joshdlucas@gmail.com>
2026-07-09 23:08:44 -0500

    Fix import error again.

--- src/keycap/keycap.py
+++ src/keycap/keycap.py
@@ -3,7 +3,10 @@ from dataclasses import dataclass, field
 from enum import IntFlag, auto
 
 from build123d import *
-from dataclass_wizard.mixins.yaml import YAMLWizard
+try:
+    from dataclass_wizard import YAMLWizard
+except ImportError:
+    from dataclass_wizard.mixins.yaml import YAMLWizard
 
 try:
     from bd_keyboard.src.common import *
